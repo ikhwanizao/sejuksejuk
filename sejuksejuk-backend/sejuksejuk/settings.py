976ce7f1b52a8501fcbb965f4297ff195c03f83d
Ikhwan <ikhwanm32@gmail.com>
2026-05-24 02:05:44 +0800

    feat: Update database configuration to use DATABASE_URL for dynamic database settings

--- sejuksejuk-backend/sejuksejuk/settings.py
+++ sejuksejuk-backend/sejuksejuk/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import timedelta
 from decouple import config, Csv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,10 +70,11 @@ WSGI_APPLICATION = 'sejuksejuk.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+        'DATABASE_URL',
+        default=f"sqlite:///{str(BASE_DIR / 'db.sqlite3')}", 
+        cast=dj_database_url.parse
+    )
 }
 
 
